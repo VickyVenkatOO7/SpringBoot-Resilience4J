66bc353684a108900c4a439cc5ac16314807a3f9
Trinadh Sai Rapeti <116419508+VickyVenkatOO7@users.noreply.github.com>
2025-05-10 03:17:54 +0530

    Add files via upload
    Implemented TimeLimiter using Resilience4j.

---
+++ TimeLimiter/catalog-service/src/main/java/com/example/demo/controller/CatalogServiceController.java
@@ -0,0 +1,49 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.Order;
+import com.example.demo.repository.OrderRepo;
+
+import jakarta.annotation.PostConstruct;
+
+@RestController
+@RequestMapping("/orders")
+public class CatalogServiceController {
+	
+	@Autowired
+    private OrderRepo orderRepo;
+
+    @PostConstruct
+    public void initOrdersTable() {
+        orderRepo.saveAll(Stream.of(
+                        new Order("mobile", "electronics", "white", 20000),
+                        new Order("T-Shirt", "clothes", "black", 999),
+                        new Order("Jeans", "clothes", "blue", 1999),
+                        new Order("Laptop", "electronics", "gray", 50000),
+                        new Order("digital watch", "electronics", "black", 2500),
+                        new Order("Fan", "electronics", "black", 50000)
+                ).
+                collect(Collectors.toList()));
+    }
+
+	@GetMapping
+	public List<Order> getOrders() throws InterruptedException{
+		Thread.sleep(5000);
+		return orderRepo.findAll();
+	}
+	
+	@GetMapping("/{category}")
+	public List<Order> getOrdersByCategory(@PathVariable("category") String category){
+		return orderRepo.findByCategory(category);
+	}
+
+}
---
+++ TimeLimiter/user-service/src/main/java/com/example/demo/controller/UserClientController.java
@@ -0,0 +1,63 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.example.demo.entity.OrderDTO;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
+
+@RestController
+@RequestMapping("/user-service")
+public class UserClientController {
+	
+	@Autowired
+    @Lazy
+    private RestTemplate restTemplate;
+
+    public static final String USER_SERVICE="userService";
+
+    private static final String BASEURL = "http://localhost:9191/orders";
+
+    private int attempt = 1;
+
+
+    @GetMapping("/displayOrders")
+    @TimeLimiter(name = USER_SERVICE, fallbackMethod = "getAllAvailableProducts")
+    public CompletableFuture<List<OrderDTO>> displayOrders(@RequestParam("category") String category) {
+    	return CompletableFuture.supplyAsync(() -> {
+    		String url = (category == null || category.isEmpty()) ? BASEURL : BASEURL + "/" + category;
+            System.out.println("TimeLimiter method called " + attempt++ + " times " + "on " + new Date());
+            return restTemplate.getForObject(url, ArrayList.class);
+    	});
+    }
+
+
+    public CompletableFuture<List<OrderDTO>> getAllAvailableProducts(Exception e){
+    	System.out.println("Fallback due to RateLimiter: " + e.getMessage());
+        List<OrderDTO> fallbackOrders = Stream.of(
+                new OrderDTO(119, "LED TV", "electronics", "white", 45000),
+                new OrderDTO(345, "Headset", "electronics", "black", 7000),
+                new OrderDTO(475, "Sound bar", "electronics", "black", 13000),
+                new OrderDTO(574, "Puma Shoes", "foot wear", "black & white", 4600),
+                new OrderDTO(678, "Vegetable chopper", "kitchen", "blue", 999),
+                new OrderDTO(532, "Oven Gloves", "kitchen", "gray", 745)
+        ).collect(Collectors.toList());
+        return CompletableFuture.completedFuture(fallbackOrders);
+    }
+}
